7d1b01a2c536f87c1fe36080f3aec81115c31961
Alex Lee <alexanderbevislee@gmail.com>
2025-08-14 15:26:24 -0700

    basic hangman minigame

---
+++ Assets/Scripts/Minigames/HangmanMinigame.cs
@@ -0,0 +1,57 @@
+using TMPro;
+using UnityEngine;
+
+public class HangmanMinigame : Minigame
+{
+    [SerializeField] private string _word;
+    [SerializeField] private TextMeshProUGUI _text;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        string hiddenText = "";
+
+        foreach (var ch in _word)
+        {
+            if (ch == ' ')
+            {
+                hiddenText += " ";
+            }
+            else
+            {
+                hiddenText += "_";
+            }
+        }
+
+        _text.text = hiddenText;
+    }
+
+    private void Update()
+    {
+        if (string.IsNullOrEmpty(Input.inputString))
+        {
+            return;
+        }
+
+        foreach (char c in Input.inputString)
+        {
+            char[] chars = _text.text.ToCharArray();
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (_word.ToLower()[i] == c)
+                {
+                    chars[i] = c;
+                }
+            }
+
+            _text.text = new string(chars);
+        }
+    }
+
+    protected override void FinishGame()
+    {
+        base.FinishGame();
+    }
+
+}
