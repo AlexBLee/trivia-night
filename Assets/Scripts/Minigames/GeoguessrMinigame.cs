11039be88c52be918b8920ab0094978cf6a04a6c
Alex Lee <alexanderbevislee@gmail.com>
2025-09-03 11:45:46 -0700

    geoguessr working

--- Assets/Scripts/Minigames/GeoguessrMinigame.cs
+++ Assets/Scripts/Minigames/GeoguessrMinigame.cs
@@ -4,12 +4,16 @@ using UnityEngine.UI;
 public class GeoguessrMinigame : Minigame
 {
     [SerializeField] private Button _finishButton;
+    [SerializeField] private Image _image;
 
     public override void Initialize(MinigameData minigameData)
     {
         base.Initialize(minigameData);
         SendMessage("geoguessr");
         _finishButton.onClick.AddListener(FinishGame);
+
+        var image = Resources.Load<Sprite>(minigameData.Input);
+        _image.sprite = image;
     }
 
     protected override void ReceiveMessage(string message)
