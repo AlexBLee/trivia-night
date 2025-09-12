a40e30e22f2662852678d34e1488e3d2cbb8adab
Alex Lee <alexanderbevislee@gmail.com>
2025-09-12 16:35:30 -0700

    got final jeopardy working

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -3,9 +3,20 @@ using UnityEngine;
 public class UIManager : MonoBehaviour
 {
     [SerializeField] private GameObject _gameSelectionContainer;
+    [SerializeField] private GameObject[] _finalJeopardyButtons;
 
     public void ShowGameSelection(bool show)
     {
         _gameSelectionContainer.gameObject.SetActive(show);
+    }
+
+    public void ShowFinalJeopardy(bool show)
+    {
+        _gameSelectionContainer.gameObject.SetActive(!show);
+
+        foreach (var button in _finalJeopardyButtons)
+        {
+            button.SetActive(show);
+        }
     }
 }
