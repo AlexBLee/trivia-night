3beceee6d01057f2d469bd47440d87aa231b29eb
Alex Lee <alexanderbevislee@gmail.com>
2025-11-20 13:54:19 -0800

    marker/button/map improvements on site

--- react-ui/src/GeoguessrGame.jsx
+++ react-ui/src/GeoguessrGame.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef, useMemo, useCallback } from 'react';
 import { useWebSocket } from './WebsocketServer';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, useMapEvents } from 'react-leaflet';
 import './App.css';
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
@@ -22,45 +22,26 @@ const center = {
   lng: -122.940801,
 }
 
-function DraggableMarker({position, setPosition}) {
-  const [draggable, setDraggable] = useState(false)
-  const markerRef = useRef(null)
+function LocationMarker({position, setPosition}) {
+  const map = useMapEvents({
+    click(e) {
+      setPosition(e.latlng);
+    },
+  });
 
-  const eventHandlers = useMemo(
-    () => ({
-      dragend() {
-        const marker = markerRef.current
-        if (marker != null) {
-          setPosition(marker.getLatLng())
-        }
-      },
-    }),
-    [setPosition],
-  )
-
-  const toggleDraggable = useCallback(() => {
-    setDraggable((d) => !d)
-  }, [])
-
-  return (
-    <Marker
-      draggable={draggable}
-      eventHandlers={eventHandlers}
-      position={position}
-      ref={markerRef}>
-      <Popup minWidth={90}>
-        <span onClick={toggleDraggable}>
-          {draggable
-            ? 'Marker is draggable'  
-            : 'Click here to make marker draggable'}
-        </span>
-      </Popup>
+  return position === null ? null : (
+    <Marker position={position} draggable={true} eventHandlers={{
+      dragend(e) {
+        setPosition(e.target.getLatLng());
+      }
+    }}>
     </Marker>
-  )
+  );
 }
 
 function GeoguessrGame() {
   const { socket, addMessageListener } = useWebSocket();
+  const [submitText, setSubmitText] = useState('Submit Guess');
   const [markerPosition, setMarkerPosition] = useState(center);
 
   useEffect(() => {
@@ -78,28 +59,27 @@ function GeoguessrGame() {
   const handleClick = (lat, lng) => {
     if (socket && socket.readyState === WebSocket.OPEN) {
       socket.send(`${lat},${lng}`);
+      setSubmitText('Submitted!')
     }
   };
 
   return (
     <>
       <h1>Geoguessr!</h1>
-      <div className="map">
+      <div className="map-fullscreen">
         <MapContainer center={[49.206289, -122.940801]} zoom={11}>
           <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'/>
-          <DraggableMarker position={markerPosition} setPosition={setMarkerPosition}/>
+          <LocationMarker position={markerPosition} setPosition={setMarkerPosition}/>
         </MapContainer>
-      </div>
-
-      <div className="position-display">
-        <p>Marker Position:</p>
-        <p>Latitude: {markerPosition.lat.toFixed(6)}</p>
-        <p>Longitude: {markerPosition.lng.toFixed(6)}</p>
-        <button onClick={() => 
-          handleClick
-          (markerPosition.lat.toFixed(6), 
-          markerPosition.lng.toFixed(6))}>
-            Submit</button>
+        
+        <button 
+          className="submit-button-overlay"
+          onClick={() => handleClick(
+            markerPosition.lat.toFixed(6), 
+            markerPosition.lng.toFixed(6)
+          )}>
+          {submitText}
+        </button>
       </div>
     </>
   );
